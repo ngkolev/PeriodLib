a7b4d8be0049117a45b2f660d28e6c2767839209
niko <nikolay.kolev@icb.bg>
2014-12-28 17:11:12 +0200

    PeriodCollectionTest

--- PeriodLib.Test/PeriodCollectionTest.cs
+++ PeriodLib.Test/PeriodCollectionTest.cs
@@ -10,6 +10,137 @@ namespace PeriodLib.Test
     [TestClass]
     public class PeriodCollectionTest
     {
+        [TestMethod]
+        public void Can_create_PeriodCollection_using_copy_constructor()
+        {
+            Assert.Fail();
+        }
 
+        [TestMethod]
+        public void Copy_constructor_throws_an_exception_if_the_argument_is_null()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Can_create_PeriodCollection_using_generic_constructor()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsEmpty_returns_true_if_the_collection_is_empty()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsEmpty_returns_false_if_the_collection_is_not_empty()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Length_returns_the_sum_of_the_lengths_of_the_periods()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetSet_returns_set_of_periods()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsOverlappingWith_returns_true_if_the_periods_are_overlapping()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsOverlappingWith_returns_false_if_the_periods_are_not_overlapping()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsOverlappingWith_returns_false_if_the_argument_collection_is_empty()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void IsOverlappingWith_throws_an_exception_if_the_argument_is_null()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetDifference_get_the_difference_between_two_PeriodCollections()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetDifference_throws_an_exception_if_the_argument_is_null()
+        {
+            Assert.Fail();
+        }
+
+
+        [TestMethod]
+        public void GetIntersection_get_the_intersection_between_two_PeriodCollections()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetIntersection_throws_an_exception_if_the_argument_is_null()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Equals_returns_false_if_the_other_object_is_not_a_period_collection()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Equals_returns_false_if_the_other_object_is_null()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Equals_period_collection_returns_true_for_equal_period_collections()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Equals_period_collection_returns_false_for_not_equal_period_collections()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Equals_period_collection_throws_an_exception_if_the_argument_is_null()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetHashCode_returns_same_value_for_period_collections_that_are_equal()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void ToString_returns_correctly_formated_period_collection()
+        {
+            Assert.Fail();
+        }
     }
 }
